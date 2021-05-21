b84c20470c3a28bc8d32a259599739cb9bcdf4f8
kingsleyshaklebolt <kingsleyshaklebolt@ministryofmagic.com>
2021-05-21 12:34:22 +0200

    Adding the gitbonansa

---
+++ Gitbonansa.R
@@ -0,0 +1,10 @@
+library(usethis)
+
+library(usethis)
+use_git_config(
+  user.name = "TageVestlie", 
+  user.email = "tve008@gmail.com"
+)
+use_git()
+use_github()
+create_from_github("TageVestlie/302")
