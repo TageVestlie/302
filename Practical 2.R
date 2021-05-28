6f2e3a25cd27f9e9e2e4ee8d0db391df44edfac2
kingsleyshaklebolt <kingsleyshaklebolt@ministryofmagic.com>
2021-05-28 11:51:18 +0200

    28/05 practical 2 day

---
+++ Practical 2.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+library(palmerpenguins)
+
+
+#1. Import the penguin data from the `palmerpenguin` package
+new_penguins<- penguins%>% 
+  drop_na()
+View(new_penguins)
+
+#Do big birds have big beaks? Fit a linear model between bill length and body mass, and interpret the coefficients.
+
+new_penguins %>% 
+  ggplot(aes(x=body_mass_g,y = bill_length_mm))+
+  geom_point() +
+  stat_smooth(method = "lm" , col = "red") +
+  labs(x= "Body mass(g)", y= "bill length(mm)" )
+
+fit_lm1 <- lm(bill_length_mm~body_mass_g, data = new_penguins)
+summary(fit_lm1)
+
+
+#3. Examine the diagnostics plots. Is everything OK?#
+x11()
+par(mfrow=c(2,2))
+plot(fit_lm1)
+
+
+#3. Calculate the residual sum of squares 
+#(find sum the squared difference between the estimates calculated from the coefficients and the observed values).#
+sum(resid(fit_lm1)^2)#shortcut
+sum((new_penguins$bill_length_mm-((2.715*10^1)+((4.003*10^-3)*new_penguins$body_mass_g)))^2, na.rm=TRUE)#full calculation
+
+#beta0 = intercept 1 estimate
+#beta 1 = estumate body_mass
+#y= bill_length 
+#x = bodymass 
+#Add species to the model as a predictor
+new_penguins %>% 
+  ggplot(aes(x=body_mass_g,y = bill_length_mm, fill=species))+
+  geom_point() +
+  stat_smooth(method = "lm" , col = "red") +
+  labs(x= "Body mass(g)", y= "bill length(mm)" )
+
+fit_lm2 <- lm(bill_length_mm~body_mass_g*species, data = new_penguins)
+summary(fit_lm2)
